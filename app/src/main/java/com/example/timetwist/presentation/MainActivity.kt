8e283807c6f6cb129dee2206d84be425259fe9c8
champgm <2091382+champgm@users.noreply.github.com>
2023-10-11 18:21:32 -0400

    Initial commit

---
+++ app/src/main/java/com/example/timetwist/presentation/MainActivity.kt
@@ -0,0 +1,105 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
+ * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
+ * changes to the libraries and their usages.
+ */
+
+package com.example.timetwist.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import com.example.timetwist.R
+import com.example.timetwist.presentation.theme.TimeTwistTheme
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.wear.compose.material.Button
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WearApp("Android")
+        }
+    }
+}
+
+@Composable
+fun WearApp(greetingName: String) {
+    TimeTwistTheme {
+        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
+         * version of LazyColumn for wear devices with some added features. For more information,
+         * see d.android.com/wear/compose.
+         */
+        val (timeLeft, setTimeLeft) = remember { mutableStateOf(30) }
+        val (isRunning, setIsRunning) = remember { mutableStateOf(false) }
+        androidx.compose.runtime.LaunchedEffect(key1 = isRunning) {
+            if (isRunning) {
+                while (timeLeft > 0) {
+                    delay(1000)
+                    setTimeLeft(timeLeft - 1)
+                }
+                setIsRunning(false)
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+
+            Text(
+                textAlign = TextAlign.Center,
+                text = "$timeLeft seconds left"
+            )
+            Button(
+                onClick = {
+                    if (!isRunning) {
+                        setIsRunning(true)
+                    }
+                }) {
+                Text(text = if (isRunning) "Timer Running" else "Start Timer")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(greetingName: String) {
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colors.primary,
+        text = stringResource(R.string.hello_world, greetingName)
+    )
+}
+
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    WearApp("Preview Android")
+}
